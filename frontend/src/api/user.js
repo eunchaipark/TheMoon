0f49c0dc42a0b703950692a925bed1a69b27774f
eunchaipark <qkrdmsco578198@gmail.com>
2026-06-09 12:18:14 +0900

    - 개인별 카테고리 선호도 백엔드 호출 함수 설정

--- frontend/src/api/user.js
+++ frontend/src/api/user.js
@@ -1,13 +1,24 @@
-// TODO: 백엔드 구성 후 실제 API로 교체
+import axios from 'axios'
+
+const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000'
+const api = axios.create({baseURL: BASE_URL})
+
+api.interceptors.request.use(config => {
+    const token = localStorage.getItem('token')
+    if (token) config.headers.Authorization = `Bearer ${token}`
+    return config
+})
 
 export const getCategoryPrefs = async () => {
-  return [
-    { category_id: 1, weight: 5 },
-    { category_id: 2, weight: 5 },
-    { category_id: 3, weight: 5 },
-  ]
+    const {data} = await api.get('/users/prefs/categories')
+    return data
 }
 
 export const updateCategoryPrefs = async (prefs) => {
-  return { success: true }
+    const payload = Object.entries(prefs).map(([category_id, weight]) => ({
+        category_id: Number(category_id),
+        weight
+    }))
+    const {data} = await api.post('/users/prefs/categories', payload)
+    return data
 }
